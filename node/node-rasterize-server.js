ad1098fdc4ba063c4d99009f3b73797c14bfb65a
Matias Figueroa <matixfigueroa@gmail.com>
2013-04-26 16:04:26 -0300

    More naming imprevements. Simplified rasterize script.

--- node/node-rasterize-server.js
+++ node/node-rasterize-server.js
@@ -8,10 +8,10 @@ var app = connect();
 app.use(connect.query());
 app.use("/rasterize", function (req, res) {
   var url = req.query.q,
-      dest = "tmp/" + url.replace(/\/|\.|\?|\=|\&|\%/g, "_").replace(/\:/g, "") + ".png";
+      dest = "tmp/" + url.replace(/\s|\/|\.|\?|\=|\&|\%/g, "_").replace(/\:/g, "") + ".png";
 
   if(url) {
-    console.log("phantomjs", ["phantomjs/rasterize.js", url, dest])
+    console.log("phantomjs", "phantomjs/rasterize.js", url, dest)
     var p = exec("phantomjs", ["phantomjs/rasterize.js", url, dest]);
     p.stdout.pipe(process.stdout);
     p.stderr.pipe(process.stderr);
