ef5d0f86fcde78572fa2c4537337dc2477259292
Matias Figueroa <matixfigueroa@gmail.com>
2013-04-26 00:54:19 -0300

    Fixed shell server "cd" command bug caused by the multi-process approach.

--- node/node-shell-server.js
+++ node/node-shell-server.js
@@ -1,6 +1,7 @@
 var http  = require('http'),
     io    = require('socket.io'),
     util  = require('util'),
+    path  = require('path'),
     spawn = require('child_process').exec;
  
 var server = http.createServer(function(req, res){
@@ -9,13 +10,38 @@ var server = http.createServer(function(req, res){
 }).listen(8002);
  
 var io_app  = io.listen(server),
+    cwd = process.cwd(),
     currentProcess;
+
+function getUserHome() {
+  return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+}
 
 io_app.sockets.on('connection', function(socket){
   socket.on('exec', function(cmd){
+
     socket.emit("exec_init");
+
+    var cdm;
+    if(cdm = cmd.match(/^\s*cd (.*)/)){
+      cdm[1] = cdm[1].replace("~", getUserHome());
+      var cwdt = path.resolve(cwd, cdm[1]);
+      if(path.existsSync(cwdt)) {
+        cwd = cwdt;
+        socket.emit("stdout", cwd);
+      }
+      else {
+        socket.emit("stderr", cwdt +" is not a valid path.");
+      }
+
+      socket.emit("exec_end");
+      return;
+    }
+
     if(!currentProcess){
-      currentProcess = spawn(cmd);
+      currentProcess = spawn(cmd, {
+        cwd: cwd
+      });
 
       currentProcess.stdout.on('data', function(data) {
         socket.emit("stdout", String(data));
